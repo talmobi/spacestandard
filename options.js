1f65bf14d5fa31ab1fbdcb8c2d3f86655831a7c9
Christian Talmo <talmo.christian@gmail.com>
2017-09-07 23:40:07 +0300

    smol fixes

--- options.js
+++ options.js
@@ -8,8 +8,8 @@ module.exports = {
   homepage: pkg.homepage,
   bugs: pkg.bugs.url,
   eslint: eslint, // pass any version of eslint >= 1.0.0
-  cmd: 'omnistandard', // should match the "bin" key in your package.json
-  tagline: 'Live by your own standards!', // displayed in output --help
+  cmd: 'standarder', // should match the "bin" key in your package.json
+  tagline: 'more standard, standarder', // displayed in output --help
   eslintConfig: {
     configFile: path.join( __dirname, 'eslintrc.json' )
   },
