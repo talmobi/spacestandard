587c8198a18ea575fb1167f78491fb1c4c2c05d8
Christian Talmo <talmo.christian@gmail.com>
2017-09-09 04:05:11 +0300

    renamed to a more descriptive name

--- options.js
+++ options.js
@@ -8,8 +8,8 @@ module.exports = {
   homepage: pkg.homepage,
   bugs: pkg.bugs.url,
   eslint: eslint, // pass any version of eslint >= 1.0.0
-  cmd: 'standarder', // should match the "bin" key in your package.json
-  tagline: 'more standard, standarder', // displayed in output --help
+  cmd: 'spacestandard', // should match the "bin" key in your package.json
+  tagline: 'standard with spaces, spacestandard', // displayed in output --help
   eslintConfig: {
     configFile: path.join( __dirname, 'eslintrc.json' )
   },
